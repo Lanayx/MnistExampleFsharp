996a865ca83c9ab73cad410e1dfec4508ea9874c
ODIN\odind <odindafna2006@rambler.ru>
2016-09-13 12:59:55 +0300

    Small update

--- Program.fs
+++ Program.fs
@@ -1,13 +1,14 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
-//module Program
+//    var net = new Network(new[] { 784, 30, 10 });
+//    var data = DataLoader.Load();
+//     net.SGD(data.Item1, 30, 10, 1.0, data.Item2);
+module Program
 
 open MathNet.Numerics
 
 [<EntryPoint>]
 let main argv =
     Control.UseNativeMKL()
-//    var net = new Network(new[] { 784, 30, 10 });
-//    var data = DataLoader.Load();
-//     net.SGD(data.Item1, 30, 10, 1.0, data.Item2);
-    0 // return an integer exit code
+    let data = DataLoader.load()
+    0 
