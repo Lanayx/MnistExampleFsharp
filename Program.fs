efa707e6767d0063cb7fee26919188c2ef9ca4d5
ODIN\odind <odindafna2006@rambler.ru>
2016-09-13 11:15:43 +0300

    Initial commit

--- Program.fs
+++ Program.fs
@@ -1,7 +1,13 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
+//module Program
+
+open MathNet.Numerics
 
 [<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
+let main argv =
+    Control.UseNativeMKL()
+//    var net = new Network(new[] { 784, 30, 10 });
+//    var data = DataLoader.Load();
+//     net.SGD(data.Item1, 30, 10, 1.0, data.Item2);
     0 // return an integer exit code
